e5d226509da8b2e8e5409e528b7536e99ffb5471
Marco Wence <mwencehe@ucsc.edu>
2017-06-05 22:11:08 -0700

    several Jsons

--- Transplant/game/js/states/load.js
+++ Transplant/game/js/states/load.js
@@ -35,7 +35,7 @@ var load = {
 		game.load.image('bigLight', 'bigLight.png');
 		game.load.image('cabinet', 'filingCabinet.png');
 		game.load.image('keyCard303', 'keyCard303.png');
-		game.load.image('bookShelf', 'bookShelf.jpg');
+		game.load.image('bookShelf', 'bookShelf.jpg');3
 		game.load.image('bed', 'hospitalBed.png');
 		game.load.image('medBox', 'MedBox.png');
 		game.load.image('labDoor', 'labDoor.png');
@@ -44,8 +44,18 @@ var load = {
 		game.load.path = '../game/data/';
 		game.load.json('level0', 'Level0-StartRoom.json');
 		game.load.json('level1', 'Level1-Hallway.json');
+		game.load.json('level2', 'Level2-HallwayF2.json');
+		game.load.json('level3', 'Level3-HallwayF1.json');
+		game.load.json('labLevel', 'Level4-lab.json');
 		game.load.json('endLevel', 'endLevel.json');
 		game.load.json('room303', 'room303.json'); 
+		game.load.json('room305', 'room305.json');
+		game.load.json('doctorOffice', 'doctorOffice.json');
+		game.load.json('room203', 'room203.json');
+		game.load.json('room205', 'room205.json');
+		game.load.json('room101', 'room101.json');
+		game.load.json('room103', 'room103.json');
+		game.load.json('room105', 'room105.json');
 
 		// sound
 		game.load.path = '../game/assets/audio/';
