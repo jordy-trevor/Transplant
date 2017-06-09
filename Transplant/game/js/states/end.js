4e51d69844a74094114e3d4e1b6990c180a93839
MarcoWence <mwencehe@ucsc.edu>
2017-06-09 04:10:52 -0700

    finished level3 JSON

--- Transplant/game/js/states/end.js
+++ Transplant/game/js/states/end.js
@@ -1,12 +1,12 @@
 var endState = {
 	create: function(){
 		var end = game.add.tileSprite(0,0,1200,800, 'gameOver');
-		var button = game.add.button(1055, 555, 'playButton', this.actionOnClick, this);
-		button.scale.setTo(1.65,0.5);
-		button.alpha = 0;
+		var button = game.add.button(1055, 535, 'menuButton', this.actionOnClick, this);
+		//button.scale.setTo(1.65,0.5);
+		//button.alpha = 0;
 
 		//Make transparent
-		end.alpha = 0;
+		//end.alpha = 0;
 
 		//Adding fade effects
 		game.add.tween(end).to( {alpha: 1}, 2000, Phaser.Easing.Linear.None, true, 0, 0, false);
